d8ed737a3fa8b99051ac459c6a8a329f68cd52ae
Luke Wright <lukewright1997@gmail.com>
2014-01-07 23:22:26 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Wakeup
+
+Small personal utility to help me get better sleep. Naturally made while procrastinating from both school and sleep.
+
+Sleep cycles are about 90 minutes long, and it takes about 15 minutes to go to sleep. What this code does is it takes the current time and tells you when to get up if you crawl into bed right now.
+
+If you want to install I'd recommend doing something like this.
+
+    chmod +x wakeup.py
+    cp wakeup.py /usr/local/bin/wakeup
+
+    wakeup
---
+++ wakeup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from datetime import datetime, timedelta
+
+def get_wakeups():
+  # Avg sleep cycle is 1.5 hours
+  sleep_cycles = [3, 4.5, 6, 7.5, 9, 10.5]
+  # Takes a person ~ 15 minutes to fall asleep
+  account_for_falling = datetime.now() + timedelta(minutes=15)
+
+  wakeup_times = []
+  for i in sleep_cycles:
+    wakeup_times.append(account_for_falling + timedelta(hours=i))
+
+  return wakeup_times
+
+def print_wakeups(times):
+  print "You should get up at one of the following times:"
+  for x in times:
+    print '{:%I:%M %p}'.format(x)
+
+if __name__ == '__main__':
+  print_wakeups(get_wakeups())
