522b6edf5f307f47600f3bb698e149b33d8fdcf0
Luke Wright <lukewright1997@gmail.com>
2014-01-08 23:12:20 -0600

    Python 3

--- wakeup.py
+++ wakeup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from datetime import datetime, timedelta
 
@@ -15,9 +15,9 @@ def get_wakeups():
   return wakeup_times
 
 def print_wakeups(times):
-  print "You should get up at one of the following times:"
+  print("You should get up at one of the following times:")
   for x in times:
-    print '{:%I:%M %p}'.format(x)
+    print('{:%I:%M %p}'.format(x))
 
 if __name__ == '__main__':
   print_wakeups(get_wakeups())
